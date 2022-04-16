637e9f320feaa2df240b1ef364d9f65b7bd69857
Minh Thông <thongnmfx15214@funix.edu.vn>
2022-04-16 16:01:37 +0700

    lab03.2

--- FUNiXReactJSSample-main/src/App.js
+++ FUNiXReactJSSample-main/src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 import './App.css';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { dishes: DISHES };
+  }
 
   render() {
     return (
-      <div className="App">
+      <div>
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
