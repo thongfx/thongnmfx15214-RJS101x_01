59330d4839fe6e55b4ce485bc641574e1b9e926f
Minh Thông <thongnmfx15214@funix.edu.vn>
2022-04-16 17:58:45 +0700

    props add.js

--- FUNiXReactJSSample-main/src/App.js
+++ FUNiXReactJSSample-main/src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
+import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import StaffList from './components/StaffListComponent';
 import {STAFFS} from './shared/staffs'
-import './App.css';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      staffs: STAFFS
+    }
+  }
 
   render() {
     return (
-      <div className="App">
+      <div>
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <StaffList staffs={this.state.staffs}/>
       </div>
     );
   }
