b1d172b239e894b0fee8c6fa00a80fdf2004d7ca
Minh Thông <thongnmfx15214@funix.edu.vn>
2022-04-16 18:04:28 +0700

    install dateformat

---
+++ FUNiXReactJSSample-main/src/components/StaffListComponent.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class StaffList extends Component {
+    
+    render() {
+        const menu = this.state.dishes.map((dish) => {
+            return (
+              <div key={dish.id} className="col-12 mt-5">
+                <Media tag="li">
+                  <Media left middle>
+                      <Media object src={dish.image} alt={dish.name} />
+                  </Media>
+                  <Media body className="ml-5">
+                    <Media heading>{dish.name}</Media>
+                    <p>{dish.description}</p>
+                  </Media>
+                </Media>
+              </div>
+            );
+        });
+
+        return (
+          <div className="container">
+            <div className="row">
+              <Media list>
+                  {menu}
+              </Media>
+            </div>
+          </div>
+        );
+    }
+}
+
+export default Menu;
