ad9cd053cd0d195e7251675f9a7ed0a9930713aa
Minh Thông <thongnmfx15214@funix.edu.vn>
2022-04-17 14:58:48 +0700

    add selectstaff

--- FUNiXReactJSSample-main/src/components/StaffListComponent.js
+++ FUNiXReactJSSample-main/src/components/StaffListComponent.js
@@ -3,16 +3,46 @@ import { Card, CardImg, CardTitle, CardBody, CardText } from "reactstrap";
 import dateFormat from "dateformat";
 
 class StaffList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectstaff: null,
+        };
     }
-    
+    staffselect(staff) {
+        this.setState({
+            selectstaff: staff
+        });
+    }
+    renderStaff(staff) {
+    if (staff != null) {
+      return (
+        <div className="col-12">
+          <Card>
+            <CardImg width="100%" src={staff.image} alt={staff.name} />
+            <CardBody>
+              <CardTitle>Họ và tên: {staff.name}</CardTitle>
+              <CardText>
+                Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}
+              </CardText>
+              <CardText>
+                Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}
+              </CardText>
+              <CardText>Phòng ban: {staff.department.name}</CardText>
+              <CardText>Số ngày nghỉ còn lại: {staff.annualLeave}</CardText>
+              <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>
+            </CardBody>
+          </Card>
+        </div>
+      );
+    } else {
+      return <div></div>;
+    }
+  }
   render() {
     const staffList = this.props.staffs.map((staff) => {
       return (
-        <div className={"col-12 col-md-6 col-lg-4 mt-3 "}>
+          <div className={"col-12 col-md-6 col-lg-4 mt-3 "} onClick={() => { this.staffselect(staff) }}>
           <Card key={staff.id}>
             <CardBody>
               <CardTitle>{staff.name}</CardTitle>
@@ -24,6 +54,9 @@ class StaffList extends Component {
     return (
       <div className="container">
             <div className="row">{staffList}</div>
+            <div className="row mt-5">
+                {this.renderStaff(this.state.selectstaff)}
+            </div>
       </div>
     );
   }
