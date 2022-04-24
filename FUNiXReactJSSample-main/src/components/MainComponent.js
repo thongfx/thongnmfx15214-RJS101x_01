3868cd09e758a9cb82a87c4f34315e4084ff4c05
Minh Thông <thongnmfx15214@funix.edu.vn>
2022-04-24 23:55:53 +0700

    lab05.3

--- FUNiXReactJSSample-main/src/components/MainComponent.js
+++ FUNiXReactJSSample-main/src/components/MainComponent.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import Header from './HeaderComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import Menu from './MenuComponent'; 
 import Dishdetail from './DishdetailComponent';
 import Footer from './FooterComponent';
-import { DISHES } from '../shared/dishes'; 
+import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 
@@ -13,16 +17,22 @@ class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        dishes: DISHES,
-        selectedDish: null
-      };
+      dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS  
+    };
   }
 
 
   render() {
      const HomePage = () => {
-      return(
-          <Home/>
+       return (
+          <Home 
+              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          />
       );
     }
     return (
@@ -31,6 +41,7 @@ class Main extends Component {
         <Switch>
           <Route path='/home' component={HomePage} />
           <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+          <Route exact path='/contactus' component={Contact} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
