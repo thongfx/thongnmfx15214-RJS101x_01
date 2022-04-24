93f6d12c998b110135fe7f1435b82867c2a5c140
Minh Thông <thongnmfx15214@funix.edu.vn>
2022-04-25 00:42:08 +0700

    lab05.4

--- FUNiXReactJSSample-main/src/components/DishdetailComponent.js
+++ FUNiXReactJSSample-main/src/components/DishdetailComponent.js
@@ -1,44 +1,68 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from "reactstrap";
 import dateFormat from 'dateformat';
+import { Link } from 'react-router-dom';
 
-function RenderDish({ selectedDish }) {
-    return (
-        <Card>
-            <CardImg top src={selectedDish.image} alt={selectedDish.name} />
-            <CardBody>
-                <CardTitle>{selectedDish.name}</CardTitle>
-                <CardText>{selectedDish.description}</CardText>
-            </CardBody>
-        </Card>
-    );
-}
-function RenderComments({ comments }) {
-    return (
-        <div>
-            <h4>Comments</h4>
-            {comments.map((comment) => {
-                return (
-                    <div key={comment._id}>
-                        <p>{comment.comment}</p>
-                        <p>--{comment.author}, {dateFormat(comment.date, "dd/mm/yyyy")}</p>
-                    </div>
-                );
-            })}
-        </div>
-        );
-}
+    function RenderDish({dish}) {
+        return(
+            <div>
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />                    
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+            );
+
+    }
+
+    function RenderComments({comments, postComment, dishId}) {
+        if (comments != null)
+            return(
+                <div>
+                    <h4>Comments</h4>
+                    <ul className="list-unstyled">
+                            {comments.map((comment) => {
+                                return (
+                                    <div in key={comment._id}>
+                                        <li>
+                                        <p>{comment.comment}</p>
+                                        <p>{comment.rating} stars</p>
+                                        </li>
+                                    </div>
+                                );
+                            })}
+                    </ul>
+                </div>
+            );
+        else
+            return(
+                <div></div>
+            );
+    }
         
-const DishDetail = ({ selectedDish }) => {
-    if (selectedDish != null)
+const DishDetail = (props) => {
+    if ( props.dish != null)
     return (
         <div className="container">
         <div className="row">
-            <div  className="col-12 col-md-5 m-1">
-                <RenderDish selectedDish={selectedDish}/>
+            <Breadcrumb>
+                <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+            </Breadcrumb>        
+            <div className="col-12">
+                <h3>{props.dish.name}</h3>
+                    <hr />
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-12 col-md-5 m-1">
+                <RenderDish dish={props.dish} />
             </div>
             <div className="col-12 col-md-5 m-1">
-                <RenderComments comments = {selectedDish.comments}/>       
+                <RenderComments comments={props.comments} />
             </div>
         </div>
         </div>
