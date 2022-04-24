95e8e9ae61c9e43cdd39b604b159816de44538d9
Minh Thông <thongnmfx15214@funix.edu.vn>
2022-04-24 23:27:08 +0700

    lab05.2

--- FUNiXReactJSSample-main/src/components/MainComponent.js
+++ FUNiXReactJSSample-main/src/components/MainComponent.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import Header from './HeaderComponent';
+import Home from './HomeComponent';
 import Menu from './MenuComponent'; 
 import Dishdetail from './DishdetailComponent';
 import Footer from './FooterComponent';
-import { DISHES } from '../shared/dishes';
+import { DISHES } from '../shared/dishes'; 
+import { Switch, Route, Redirect } from 'react-router-dom';
+
 
 class Main extends Component {
 
@@ -13,19 +16,23 @@ class Main extends Component {
         dishes: DISHES,
         selectedDish: null
       };
-    }
+  }
 
-  onDishSelect(dishId) {
-        this.setState({selectedDish: dishId});
-    }
-    
+
   render() {
+     const HomePage = () => {
+      return(
+          <Home/>
+      );
+    }
     return (
       <div>
         <Header />
-        <Menu dishes={this.state.dishes}
-            onClick={(dishId) => this.onDishSelect(dishId)}/>
-        <Dishdetail selectedDish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        <Switch>
+          <Route path='/home' component={HomePage} />
+          <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+          <Redirect to="/home" />
+        </Switch>
         <Footer />
       </div>
     );
