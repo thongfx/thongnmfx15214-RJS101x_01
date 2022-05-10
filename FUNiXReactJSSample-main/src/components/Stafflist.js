4dc5f793afac79442686a5f793c6d2112d329e76
Minh Thông <thongnmfx15214@funix.edu.vn>
2022-05-10 15:07:13 +0700

    th1

--- FUNiXReactJSSample-main/src/components/Stafflist.js
+++ FUNiXReactJSSample-main/src/components/Stafflist.js
@@ -1,61 +1,91 @@
-import React, {useState} from 'react';
-import { Card, CardImg, CardTitle } from "reactstrap";
+import React, {Component} from 'react';
+import { Card, CardImg, CardTitle, Form, FormGroup, Label, Button, Input, Col } from "reactstrap";
 import { Link } from 'react-router-dom';
 
-function RenderStafflist({staff}) {
-    return (
-        <Link to={`/Nhanvien/${staff.id}`}>
-            <CardImg width="100%" src={staff.image} alt={staff.name} />
-            <Card>
-                <CardTitle className='text-center'>{staff.name}</CardTitle>
-            </Card>
-        </Link>
-    )
-}
-
-const Stafflist = (props) => {
-    const [inputId, setInputId] = useState("");
-    let inputHandler = (e) => {
-        //input dữ liệu 
-        setInputId(e.target.value);
-    };
-
-    const filteredData = props.staffs.filter((el) => {
-        //Không có dữ liệu trả về stafflist 
-        if (inputId === "") {
-            return el;
+class Stafflist extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            findStaff: "",
         }
-        //Trả về nhân viên có id
-        else {
-            return el.id === parseInt(inputId);
-        }
-    }) 
+     
+        this.handlefindStaff = this.handlefindStaff.bind(this);
+        this.filterStaff = this.filterStaff.bind(this);
 
+    }
+   
+    filterStaff() {
+        this.setState({
+            findStaff: this.findStaff.value
+        });
+    }
 
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col-12 mt-3">
-                    <h3>Nhân Viên</h3>
-                    <hr />
+    handlefindStaff(event) {
+        this.filterStaff();
+        event.preventDefault();
+    }
+    
+    render() {
+        const stafflist = this.props.staffs
+            .filter((staff) => {
+                if (this.state.findStaff === "") {
+                    return staff;
+                }
+                else if (staff.name.toLowerCase().includes(this.state.findStaff.toLowerCase())) {
+                    return staff;
+                } else {
+                    return 0;
+                }
+            } )
+            .map((staff) => {
+            return (
+                <div className="col-6 col-md-4 col-lg-2 mb-3">
+                    <Link to={`/Nhanvien/${staff.id}`}>
+                        <CardImg width="100%" src={staff.image} alt={staff.name} />
+                        <Card>
+                            <CardTitle className='text-center'>{staff.name}</CardTitle>
+                        </Card>
+                    </Link>
                 </div>
-                <div class="form-outline mb-3">
-                    <input
-                        class="form-control"
-                        type="number" min="0"
-                        onChange={inputHandler}
-                        placeholder="Tìm nhân viên theo Id"
-                    />
+            )
+        });
+
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-md-6 mt-3">
+                        <div className="row">
+                            <div className='col-10 col-md-8'>
+                                <h3>Nhân Viên</h3>
+                            </div>
+                            <div className='col-2 col-md-4'>
+                                <Button>
+                                    <span className='fa fa-plus fa-lg'></span>
+                                </Button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-12 col-md-6 mt-3'>
+                        <Form onSubmit={this.handlefindStaff}>
+                            <FormGroup row >
+                                <div className='col-10 col-md-10'>
+                                    <Input type="text" id="findStaff" name="findStaff"
+                                        innerRef={(input) => this.findStaff = input} />
+                                </div>
+                                <div className='col-2 col-md-2'>
+                                    <Button type="submit" value="submit" color="primary">Tìm</Button>                                              
+                                </div>
+                            </FormGroup>    
+                        </Form>    
+                    </div>
+                </div>
+                <hr/>
+                <div className='row'>
+                    {stafflist}
                 </div>
             </div>
-            <div className="row">
-                {filteredData.map((staff) => (
-                    <div className="col-6 col-md-4 col-lg-2 mb-3 " key={staff.id}>
-                        <RenderStafflist staff={staff} />
-                    </div>))}
-            </div>
-        </div>
-    );
+        )
+    }
 }
 
 export default Stafflist;
