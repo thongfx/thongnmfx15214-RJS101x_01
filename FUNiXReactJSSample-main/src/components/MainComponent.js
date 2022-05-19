b1a57c5ed60d479ce64ef59798f8361ff75d567e
Minh Thông <thongnmfx15214@funix.edu.vn>
2022-05-19 10:04:03 +0700

    lab08.2

--- FUNiXReactJSSample-main/src/components/MainComponent.js
+++ FUNiXReactJSSample-main/src/components/MainComponent.js
@@ -8,6 +8,8 @@ import Dishdetail from './DishdetailComponent';
 import Footer from './FooterComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux';
+import { addComment } from '../redux/ActionCreators';
+
 
 const mapStateToProps = state => {
   return {
@@ -17,6 +19,10 @@ const mapStateToProps = state => {
     leaders: state.leaders
   }
 }
+const mapDispatchToProps = dispatch => ({
+    addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment))
+  
+  });
 
 class Main extends Component {
 
@@ -38,7 +44,9 @@ class Main extends Component {
     const DishWithId = ({match}) => {
       return(
           <Dishdetail dish= {this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
-            comments = {this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+          comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
+          addComment={this.props.addComment}
+        />
       );
     };
     return (
@@ -58,4 +66,4 @@ class Main extends Component {
   }
 }
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
